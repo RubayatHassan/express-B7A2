cab92fe87cd272725626500e2f6e86a7dd8b4790
Quazi Rubayat Hassan <rubayat.roktim@gmail.com>
2026-05-23 23:15:09 +0600

    auth

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -4,7 +4,6 @@ import { authController } from "./auth.controller";
 const router = Router();
 
 router.post("/signup", authController.signupUser);
-router.post("/login", authController.loginUser);
-router.post("/refresh-token", authController.refreshToken);
+
 
 export const authRoute = router;
