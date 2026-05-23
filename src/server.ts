2d33b154573ebb7e81d5542e711a0fa09e9735df
Quazi Rubayat Hassan <rubayat.roktim@gmail.com>
2026-05-23 23:58:09 +0600

    main

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import app from "./app";
 import config from "./config";
 import { initDB } from "./db";
 
-initDB()
+const main = () => {
+  initDB();
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
+};
 
-export default app
+main();
