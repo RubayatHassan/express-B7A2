a65f9d0ce74cca15c038547a673a465eec0a6747
Quazi Rubayat Hassan <rubayat.roktim@gmail.com>
2026-05-23 23:16:47 +0600

    authentication

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -6,11 +6,7 @@ import { userController } from "./user.controller";
 const router = Router();
 
 router.post("/", userController.createUser);
-router.get(
-  "/",
-  auth(USER_ROLE.contributor, USER_ROLE.maintainer),
-  userController.getAllUsers,
-);
+
 router.get("/:id", userController.getSingleUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
